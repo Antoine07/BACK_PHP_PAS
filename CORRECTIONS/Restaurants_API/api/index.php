d841c16efe7a1bfd6750d29d444230b3cec24245
Antoine07 <antoine.lucsko@gmail.com>
2024-02-23 20:47:30 +0100

    api mongo

---
+++ CORRECTIONS/Restaurants_API/api/index.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once __DIR__ . '/../bootstrap.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$action = $request->query->get('action');
+$response = new Response();
+if ($action === 'cuisineItalian') {
+    $result = $collection->find(
+        [
+            'cuisine' => 'Italian',
+        ],
+        [
+            'limit' => 5,
+            'projection' => [
+                'name' => 1,
+                '_id' => 0
+            ],
+        ]
+    );
+
+    $response->setContent(json_encode(["restaurants" => $result->toArray()]));
+}
+
+if ($action === 'coffee') {
+
+    $filter = [
+        'name' => new MongoDB\BSON\Regex('coffee', 'i')
+    ];
+
+    $projection = [
+        'projection' => [
+            '_id' => 0,
+            'name' => 1
+        ]
+    ];
+
+    $options = [];
+
+    $result = $collection->find($filter, $projection);
+    $response->setContent(json_encode(["restaurants" => $result->toArray()]));
+}
+
+$response->headers->set('Content-Type', 'application/json');
+$response->send();
